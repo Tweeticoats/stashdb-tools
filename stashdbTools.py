ea06a7aa062b1a70e8927a040b0a2ef7f967e68b
tweeticoats <tweeticoats@github.com>
2021-05-19 12:46:24 +0930

    Got image upload to work, provide an object containing the file and it will provide the image id

--- stashdbTools.py
+++ stashdbTools.py
@@ -3,6 +3,10 @@ import requests
 import sys
 import json
 import re
+import base64
+import imghdr
+import mimetypes
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 
 class stashdbTools:
@@ -11,15 +15,14 @@ class stashdbTools:
         "Content-Type": "application/json",
         "Accept": "application/json",
         "Connection": "keep-alive",
-        "DNT": "1",
-        "Origin": "https://stashdb.org"
+        "Origin": "http://localhost:9998"
     }
 
 
 
     conn=None
-    url='https://stashdb.org/graphql'
-
+#    url='https://stashdb.org/graphql'
+    url='http://localhost:9998/graphql'
     def __init__(self,api_key,dbname='stash-go.sqlite'):
         self.conn = sqlite3.connect(dbname, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
         self.headers["ApiKey"]=api_key
@@ -195,6 +198,54 @@ class stashdbTools:
         result = self.__callGraphQL(query, variables)
         return result
 
+    def createStudio(self,input):
+        query="""mutation studioCreate($input: StudioCreateInput!) {
+  studioCreate(input: $input) {
+    id
+    name
+  }
+}"""
+        variables = {'input': input}
+        result = self.__callGraphQL(query, variables)
+
+
+    def createImage(self,image):
+#        query="""mutation imageCreate($input: ImageCreateInput!) {
+#  imageCreate(input: $input) {
+#    id
+#    url
+#    width
+#    height
+#  }
+#}"""
+#        print("xx"+str(image))
+#        variables = {'input': {'file':None}}
+#        req = {}
+#        req['query'] = query
+#        req['variables'] = variables
+
+#        query="""{"operationName": "AddImage", "variables": {"imageData": {"file": None}},
+#         "query": "mutation AddImage($imageData: ImageCreateInput!) {\n  imageCreate(input: $imageData) {\n    id\n    url\n    width\n    height\n    __typename\n  }\n}\n"}"""
+#
+
+#        print(json.dumps(req))
+#        response = requests.post(self.url, files=(('operations',(None,json.dumps(q)),('map',(None,'{"1":["variables.imageData.file"]}')),('1',(None,image))),headers=self.headers)
+#        response = requests.post(self.url, files={'operations':query,'map':'{"1":["variables.imageData.file"]}','1':(None,image)},headers=self.headers)
+#        print(response)
+#        query={"operationName":"AddImage","variables":{"imageData":{"file":None}},"query":"mutation AddImage($imageData: ImageCreateInput!) {\n  imageCreate(input: $imageData) {\n    id\n    url\n    width\n    height\n    __typename\n  }\n}\n"}
+
+
+        print("image:"+str(len(image)))
+        m = MultipartEncoder(fields={'operations':'{"operationName":"AddImage","variables":{"imageData":{"file":null}},"query":"mutation AddImage($imageData: ImageCreateInput!) {  imageCreate(input: $imageData) {id}}"}',
+                                     'map':'{"1":["variables.imageData.file"]}',
+                                     '1': ('1.jpg',image)})
+
+        headers_tmp = self.headers.copy()
+        headers_tmp["Content-Type"] = m.content_type
+
+        response = requests.post(self.url, data=m,headers=headers_tmp)
+        return response.json()
+
     def loopPerformers(self):
         c = self.conn.cursor()
         c.execute('select id,name from performers where id not in (select performer_id from performer_stash_ids) order by 1 asc;')
@@ -255,11 +306,46 @@ class stashdbTools:
         name=" ".join(r)
         name=name.replace("Bang","Bang!")
 
+
         res = self.queryStudio(name,per_page=100)
         if "queryStudios" in res:
             for p in res["queryStudios"]["studios"]:
                 if (p["name"]).lower().replace(" ","") == name.lower().replace(" ",""):
                     return p["id"]
+    def exportStudios(self):
+        c = self.conn.cursor()
+#        c.execute('select id,name,url,(select image from studios_image where studio_id=id) image from studios where id not in (select studio_id from studio_stash_ids) order by 1 asc;')
+        c.execute('select id,name,url,(select image from studios_image where studio_id=id) image from studios;')
+        for row in c.fetchall():
+
+            id= row[0]
+            name=row[1]
+            urls=row[2]
+            image=row[3]
+
+            input={"name":name}
+            if image is not None:
+                print("creating image?")
+                image_create=self.createImage(image)
+                if "data" in image_create:
+                    image_id=image_create["data"]["imageCreate"]["id"]
+                    input["image_ids"]=[image_id]
+            if urls is not None:
+                input["urls"]= [{"url": urls, "type": "studio"}]
+            self.createStudio(input)
+
+
+#    def uploadImage(self,image):
+#        img=self.make_image_data_url(data)
+#        res=self.createImage(data)
+#        return res
+    def make_image_data_url(self,image_data):
+        # type: (bytes,) -> str
+        img_type = imghdr.what(None, h=image_data) or 'jpeg'
+        mime = mimetypes.types_map.get('.' + img_type, 'image/jpeg')
+        encoded = base64.b64encode(image_data).decode()
+        return 'data:{0};base64,{1}'.format(mime, encoded)
+
 
 
 
@@ -268,8 +354,12 @@ if __name__ == '__main__':
         if sys.argv[1] == "performer_match":
             tools=stashdbTools(sys.argv[2],'stash-go.sqlite')
             tools.loopPerformers()
-        if sys.argv[1] == "studio_match":
+        elif sys.argv[1] == "studio_match":
             tools=stashdbTools(sys.argv[2],'stash-go.sqlite')
             tools.loopStudio()
+        elif sys.argv[1] == "export_studios":
+            tools=stashdbTools(sys.argv[2],'stash-go.sqlite')
+            tools.exportStudios()
+
 
 
